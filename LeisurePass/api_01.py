5ccce9471e7525043a81433fe3d57ec89d6b8d92
tfasuyi <77242930+tfasuyi@users.noreply.github.com>
2021-04-14 21:06:16 +0100

    Update api_01.py

--- LeisurePass/api_01.py
+++ LeisurePass/api_01.py
@@ -4,15 +4,27 @@ import requests
 def get_all_cities():
     response = requests.get("https://my-json-server.typicode.com/leisurepassgroup/SDET-interview/citys")
     response_body = response.json()
-    print("Cities are:",response_body)
+    print("All cities Includes")
+    for d in response_body:
+        city = d['title']
+        print(city)
 
 
 def get_all_attractions_for_a_city_New_York():
     response = requests.get("https://my-json-server.typicode.com/leisurepassgroup/SDET-interview/attractions")
     response_body = response.json()
-    print("New York Attractions are:", response_body)
+    print("New York Attractions are:")
+    for d in response_body:
+        attraction = d['title']
+        print(attraction)
+
+
 
 def get_all_attractions_for_a_city_New_York_type_Museum_ordered_by_desc(cityid,type):
     response = requests.get(f"https://my-json-server.typicode.com/leisurepassgroup/SDET-interview/attractions?cityId={cityid}&type={type}&_sort=type&_order=desc")
     response_body = response.json()
-    print("New York Attractions for type Museum  are:", response_body)
+    print("New York Attractions for type Museum  are:" ,response_body)
+
+
+
+
